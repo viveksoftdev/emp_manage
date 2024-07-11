a7c44188c4bb73b7dadef862fc15cf10c9c4a1b6
vivek <viveksoftdevm@gmail.com>
2024-07-11 12:27:30 +0530

    firstcommit

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# models for Employee
+
+class Employee(models.Model):
+
+    GENDER_CHOICES = (('m','Male'),
+                      ('f','Female'),
+                      ('o','Others'))
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    workExp = models.TextField()
+    qualifications = models.TextField()
+    projects = models.TextField()
+
+
+    def __str__(self):
+        return f'{self.name}'
