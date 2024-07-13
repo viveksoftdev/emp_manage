c1eedf50ed1bdd09f30148734049255e4750f315
viveksoftdev <142629285+viveksoftdev@users.noreply.github.com>
2024-07-13 12:02:13 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://emp-manage-ax11.onrender.com/']
+ALLOWED_HOSTS = ['emp-manage-ax11.onrender']
 
 
 # Application definition
